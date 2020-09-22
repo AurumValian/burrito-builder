a323b511882151bdfe54bb8a2a928c761184ca26
Ben McClung <aurumvalian@gmail.com>
2020-09-22 09:14:10 -0600

    Fix dev bugs preventing page from loading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,14 +7,20 @@ import OrderForm from '../../components/OrderForm/OrderForm';
 class App extends Component {
   constructor(props) {
     super();
+
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
     getOrders()
+      .then(orders => this.setState({orders: orders.orders}))
       .catch(err => console.error('Error fetching:', err));
   }
 
   render() {
+
     return (
       <main className="App">
         <header>
